b9621e1efbe294e61008c3f5f9b38f2c50aec546
Ayesha7622 <saddiqa7676@gmail.com>
2025-05-28 22:33:36 +0500

    mjbvf

--- lib/controller/utils/constants/app_assets/app_images/app_images.dart
+++ lib/controller/utils/constants/app_assets/app_images/app_images.dart
@@ -1,4 +1,5 @@
 class AppImages {
   static String base = 'assets/appImages/';
   static String markusSpiskImage = 'assets/appImages/markus-spiske.png';
+  static String markusSpisk2Image = 'assets/appImages/markus-spiske2.png';
 }
