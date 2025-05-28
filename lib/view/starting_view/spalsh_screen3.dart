b3c2977a1b112d8cfefa432468eeb6a2b47422c6
Ayesha7622 <saddiqa7676@gmail.com>
2025-05-28 23:11:33 +0500

    jvbfd

---
+++ lib/view/starting_view/spalsh_screen3.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:grocery/controller/component/Bold_text_widget.dart';
+import 'package:grocery/controller/component/Green_button_widget.dart';
+import 'package:grocery/controller/component/Normal_text_widget.dart';
+import 'package:grocery/controller/utils/constants/app_assets/app_images/app_images.dart';
+
+class SpalshScreen3 extends StatelessWidget {
+  const SpalshScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Expanded(
+          flex: 50,
+          child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(AppImages.element5Image))),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                BoldText(text: 'Buy Premium\nQuality Fruits'),
+                SizedBox(
+                  height: 10,
+                ),
+                NormalText(
+                    text:
+                        'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy'),
+                Spacer(),
+                GreenButton(text: 'Get Started', onTap: () {}),
+                SizedBox(
+                  height: 20,
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
