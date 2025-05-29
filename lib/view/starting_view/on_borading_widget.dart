4250a9efe386910ad3c629ba6319b0a457f8525f
Ayesha7622 <saddiqa7676@gmail.com>
2025-05-29 22:00:07 +0500

    hbddfvd

--- lib/view/starting_view/on_borading_widget.dart
+++ lib/view/starting_view/on_borading_widget.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:newgroceryapp/controller/component/Bold_text_widget.dart';
+import 'package:newgroceryapp/controller/component/Normal_text_widget.dart';
 import 'package:newgroceryapp/controller/utils/constants/app_assets/app_images/app_images.dart';
 
-class OnBoradingWidget extends StatelessWidget {
-  const OnBoradingWidget({super.key});
+class BoardingWidget extends StatelessWidget {
+  final String image;
+  final String heading;
+  final String description;
+  const BoardingWidget(
+      {super.key,
+      required this.image,
+      required this.heading,
+      required this.description});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
         children: [
-          OnBoradingWidget(
-              image: AppImages.deliveryImage,
-              heading: ' Fast Delivery',
-              description:
-                  'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy'),
-          OnBoradingWidget(
-              image: AppImages.deliveryImage,
-              heading: ' Buy Grocery',
-              description:
-                  'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy'),
-          OnBoradingWidget(
-              image: AppImages.deliveryImage,
-              heading: ' Enjoy Quality Food',
-              description:
-                  'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy')
+          Column(
+            children: [
+              Image(image: AssetImage(image)),
+              Spacer(),
+              BoldText(
+                text: heading,
+              ),
+              NormalText(text: description),
+              SizedBox(
+                height: 30,
+              )
+            ],
+          )
         ],
       ),
     );
