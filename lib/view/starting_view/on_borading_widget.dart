75f7141d8a313de222c535eb91d03a980969dd3b
Ayesha7622 <saddiqa7676@gmail.com>
2025-05-29 21:00:17 +0500

    hvhn

---
+++ lib/view/starting_view/on_borading_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:newgroceryapp/controller/utils/constants/app_assets/app_images/app_images.dart';
+
+class OnBoradingWidget extends StatelessWidget {
+  final String image;
+  final String heading;
+  final String description;
+  const OnBoradingWidget(
+      {super.key,
+      required this.image,
+      required this.heading,
+      required this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        children: [
+          OnBoradingWidget(
+              image: AppImages.deliveryImage,
+              heading: ' Fast Delivery',
+              description:
+                  'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy')
+        ],
+      ),
+    );
+  }
+}
