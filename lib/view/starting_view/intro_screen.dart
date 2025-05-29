8b60bddf4bbac225e9422c9c03e3335b4034a922
Ayesha7622 <saddiqa7676@gmail.com>
2025-05-29 22:24:22 +0500

    bmj

--- lib/view/starting_view/intro_screen.dart
+++ lib/view/starting_view/intro_screen.dart
@@ -1,29 +1,61 @@
+// import 'package:flutter/material.dart';
+// import 'package:newgroceryapp/controller/utils/constants/app_assets/app_images/app_images.dart';
+
+// class BoradingWidgetScreen extends StatelessWidget {
+//   const BoradingWidgetScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: PageView(
+//         children: [
+//           BoradingWidget(
+//               image: AppImages.deliveryImage,
+//               heading: ' Fast Delivery',
+//               description:
+//                   'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy'),
+//           BoradingWidget(
+//               image: AppImages.deliveryImage,
+//               heading: ' Buy Grocery',
+//               description:
+//                   'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy'),
+//           BoradingWidget(
+//               image: AppImages.deliveryImage,
+//               heading: ' Enjoy Quality Food',
+//               description:
+//                   'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy')
+//         ],
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:newgroceryapp/controller/utils/constants/app_assets/app_images/app_images.dart';
+import 'package:newgroceryapp/view/starting_view/on_borading_widget.dart';
 
-class BoradingWidget extends StatelessWidget {
-  const BoradingWidget({super.key});
+class BoradingWidgetScreen extends StatelessWidget {
+  const BoradingWidgetScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
         children: [
-          // BoradingWidget(
-          //     image: AppImages.deliveryImage,
-          //     heading: ' Fast Delivery',
-          //     description:
-          //         'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy'),
-          // BoradingWidget(
-          //     image: AppImages.deliveryImage,
-          //     heading: ' Buy Grocery',
-          //     description:
-          //         'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy'),
-          // BoradingWidget(
-          //     image: AppImages.deliveryImage,
-          //     heading: ' Enjoy Quality Food',
-          //     description:
-          //         'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy')
+          BoardingWidget(
+              image: AppImages.deliveryImage,
+              heading: ' Fast Delivery',
+              description:
+                  'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy'),
+          BoardingWidget(
+              image: AppImages.deliveryImage,
+              heading: ' Buy Grocery',
+              description:
+                  'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy'),
+          BoardingWidget(
+              image: AppImages.deliveryImage,
+              heading: ' Enjoy Quality Food',
+              description:
+                  'Lorem ipsum dolor sit amet, consetetur\nsadipscing elitr, sed diam nonumy')
         ],
       ),
     );
