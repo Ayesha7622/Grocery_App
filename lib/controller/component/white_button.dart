75749c355378ef41893e66a3cab1c4999dc71a7d
Ayesha7622 <saddiqa7676@gmail.com>
2025-06-03 10:21:56 +0500

    jdfjvnd

--- lib/controller/component/white_button.dart
+++ lib/controller/component/white_button.dart
@@ -4,12 +4,14 @@ import 'package:newgroceryapp/controller/utils/constants/app_assets/app_icons/ap
 import 'package:newgroceryapp/controller/utils/constants/app_colors/app_colors.dart';
 
 class WhiteButton extends StatelessWidget {
-  const WhiteButton({super.key});
+  final String text;
+  final VoidCallback onTap;
+  const WhiteButton({super.key, required this.text, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      //onTap: onTap
+      onTap: onTap,
       child: Container(
         margin: EdgeInsets.symmetric(horizontal: 18),
         height: 60,
