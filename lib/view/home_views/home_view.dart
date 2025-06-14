501f662d5d1cd9953b084d874e621e00dc583458
Ayesha7622 <saddiqa7676@gmail.com>
2025-06-14 19:58:57 +0500

    dbfvn

--- lib/view/home_views/home_view.dart
+++ lib/view/home_views/home_view.dart
@@ -1,43 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:newgroceryapp/controller/component/Normal_text_widget.dart';
 import 'package:newgroceryapp/controller/utils/constants/app_assets/app_icons/app_icons.dart';
+import 'package:newgroceryapp/controller/utils/constants/app_assets/app_images/app_images.dart';
 import 'package:newgroceryapp/controller/utils/constants/app_colors/app_colors.dart';
 
-class HomeScreenClass extends StatefulWidget {
+class HomeScreenClass extends StatelessWidget {
   const HomeScreenClass({super.key});
 
   @override
-  State<HomeScreenClass> createState() => _HomeScreenClassState();
-}
-
-class _HomeScreenClassState extends State<HomeScreenClass> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      height: 100,
-      width: 600,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: AppColors.grey2,
-      ),
-      child: Row(
+      body: Column(
         children: [
-          Icon(
-            Icons.search,
-            color: AppColors.greyColor,
+          Container(
+            height: 100,
+            width: 600,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: AppColors.grey2,
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.search,
+                  color: AppColors.greyColor,
+                ),
+                NormalText(
+                  text: 'Search keywords',
+                  textColor: AppColors.greyColor,
+                ),
+                Spacer(),
+                Icon(
+                  Icons.tune,
+                  color: AppColors.greyColor,
+                )
+              ],
+            ),
           ),
-          NormalText(
-            text: 'Search keywords',
-            textColor: AppColors.greyColor,
-          ),
-          Spacer(),
-          Icon(
-            Icons.tune,
-            color: AppColors.greyColor,
-          )
+          Container(
+              height: double.infinity,
+              width: double.infinity,
+              child: Stack(
+                children: [Image.asset(AppImages.ellaImage)],
+              ))
         ],
       ),
-    ));
+    );
   }
 }
