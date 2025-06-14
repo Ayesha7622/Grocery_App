7b52ad887e9712cbae1a3504749173d20725292c
Ayesha7622 <saddiqa7676@gmail.com>
2025-06-14 12:43:17 +0500

    ndhcbdn

--- lib/view/home_views/home_view.dart
+++ lib/view/home_views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newgroceryapp/controller/utils/constants/app_assets/app_icons/app_icons.dart';
 import 'package:newgroceryapp/controller/utils/constants/app_colors/app_colors.dart';
 
 class HomeScreenClass extends StatefulWidget {
@@ -12,17 +13,21 @@ class _HomeScreenClassState extends State<HomeScreenClass> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Container(
-            height: 100,
-            width: 600,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: AppColors.greyColor,
-            ),
-          )
-        ],
+      body: Container(
+        height: 100,
+        width: 600,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: AppColors.greyColor,
+        ),
+        child: Row(
+          children: [
+            Icon(Icons.search),
+            Text('Search keywords'),
+            Spacer(),
+            Icon(Icons.tune)
+          ],
+        ),
       ),
     );
   }
