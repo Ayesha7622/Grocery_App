8c244478e0a75c01ea841c7e52d7111fd1f35c52
Ayesha7622 <saddiqa7676@gmail.com>
2025-06-14 12:39:23 +0500

    mjhbvjf

---
+++ lib/view/home_views/home_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:newgroceryapp/controller/utils/constants/app_colors/app_colors.dart';
+
+class HomeScreenClass extends StatefulWidget {
+  const HomeScreenClass({super.key});
+
+  @override
+  State<HomeScreenClass> createState() => _HomeScreenClassState();
+}
+
+class _HomeScreenClassState extends State<HomeScreenClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Container(
+            height: 100,
+            width: 600,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: AppColors.greyColor,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
