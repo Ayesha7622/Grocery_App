97b96dae79e14154ab997faffd255bda6e9465b6
Ayesha7622 <saddiqa7676@gmail.com>
2025-06-20 21:22:31 +0500

    ghvhnv

--- lib/controller/utils/constants/app_assets/app_icons/app_icons.dart
+++ lib/controller/utils/constants/app_assets/app_icons/app_icons.dart
@@ -10,4 +10,16 @@ class AppIcons {
   static String leaf = 'assets/appIcons/leaf.png';
   static String vacuum = 'assets/appIcons/vacuum 1.png';
   static String vector = 'assets/appIcons/Vectorblue.png';
+  static String user = 'assets/appIcons/user.png';
+  static String credit = 'assets/appIcons/crediticon.png';
+  static String location = 'assets/appIcons/location (2).png';
+  static String order = 'assets/appIcons/ordericon.png';
+  static String signout = 'assets/appIcons/signouticon.png';
+  static String transaction = 'assets/appIcons/transaction.png';
+  static String favourite = 'assets/appIcons/favourite.png';
+  static String bell = 'assets/appIcons/bell.png';
+  // static String sign = 'assets/appIcons/sign-out.png';
+  // static String transaction = 'assets/appIcons/transfer-money.png';
+  // static String package = 'assets/appIcons/package.png';
+  // static String credit = 'assets/appIcons/credit-card.png';
 }
--- lib/view/home_views/contacts.dart
+++ lib/view/home_views/contacts.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:newgroceryapp/controller/component/Bold_text_widget.dart';
 import 'package:newgroceryapp/controller/component/Normal_text_widget.dart';
+import 'package:newgroceryapp/controller/utils/constants/app_assets/app_icons/app_icons.dart';
 import 'package:newgroceryapp/controller/utils/constants/app_assets/app_images/app_images.dart';
 import 'package:newgroceryapp/controller/utils/constants/app_colors/app_colors.dart';
 
+// class ChatsModel {
+//   String? name;
+//   String? icons;
+
+//   ChatsModel({required this.icons, required this.name});
+// }
+
 class ContactClass extends StatelessWidget {
   const ContactClass({super.key});
+  // List<ChatsModel> contact = [
+  //   ChatsModel(icons: AppIcons.user, name: 'About me'),
+  //   ChatsModel(icons: AppIcons.order, name: 'My Orders'),
+  //   ChatsModel(icons: AppIcons.favourite, name: 'My Favourites'),
+  //   ChatsModel(icons: AppIcons.location, name: 'My Address'),
+  //   ChatsModel(icons: AppIcons.credit, name: 'Credit Cards'),
+  //   ChatsModel(icons: AppIcons.transaction, name: 'Transactions'),
+  //   ChatsModel(icons: AppIcons.bell, name: 'Notifications'),
+  //   ChatsModel(icons: AppIcons.signout, name: 'Sign out'),
+  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +87,22 @@ class ContactClass extends StatelessWidget {
                 height: 100,
               ),
               CircleAvatar(
-                  radius: 50, backgroundImage: AssetImage(AppImages.girl)),
+                radius: 50,
+                backgroundImage: AssetImage(AppImages.girl),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: AppColors.green,
+                      radius: 20,
+                      child: Icon(
+                        Icons.camera_alt_outlined,
+                        color: AppColors.whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               BoldText(
                 text: 'Olivia Austin',
                 textColor: AppColors.blackColor,
@@ -79,7 +112,168 @@ class ContactClass extends StatelessWidget {
                   text: 'oliviaaustin@gmail.com',
                   textColor: AppColors.greyColor,
                   fontSize: 23),
-            ])
+            ]),
+            Column(
+              children: [
+                ListTile(
+                  leading: ImageIcon(
+                    AssetImage(
+                      AppIcons.user,
+                    ),
+                    color: AppColors.green,
+                  ),
+                  title: BoldText(
+                    text: 'About me',
+                    textColor: AppColors.blackColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  trailing: Icon(
+                    Icons.chevron_right,
+                    color: AppColors.greyColor,
+                    size: 45,
+                  ),
+                ),
+                ListTile(
+                  leading: ImageIcon(
+                    AssetImage(
+                      AppIcons.order,
+                    ),
+                    color: AppColors.green,
+                  ),
+                  title: BoldText(
+                    text: 'My Orders',
+                    fontSize: 20,
+                    textColor: AppColors.blackColor,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  trailing: Icon(
+                    Icons.chevron_right,
+                    color: AppColors.greyColor,
+                    size: 45,
+                  ),
+                ),
+                ListTile(
+                  leading: ImageIcon(
+                    AssetImage(AppIcons.favourite),
+                    color: AppColors.green,
+                  ),
+                  title: BoldText(
+                    text: 'My Favourite',
+                    fontSize: 20,
+                    textColor: AppColors.blackColor,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  trailing: Icon(
+                    Icons.chevron_right,
+                    color: AppColors.greyColor,
+                    size: 45,
+                  ),
+                ),
+                ListTile(
+                  leading: ImageIcon(
+                    AssetImage(AppIcons.location),
+                    color: AppColors.green,
+                  ),
+                  title: BoldText(
+                    text: 'My Address',
+                    fontSize: 20,
+                    textColor: AppColors.blackColor,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  trailing: Icon(
+                    Icons.chevron_right,
+                    color: AppColors.greyColor,
+                    size: 45,
+                  ),
+                ),
+                ListTile(
+                  leading: ImageIcon(
+                    AssetImage(AppIcons.credit),
+                    color: AppColors.green,
+                  ),
+                  title: BoldText(
+                    text: 'Credit Cards',
+                    fontSize: 20,
+                    textColor: AppColors.blackColor,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  trailing: Icon(
+                    Icons.chevron_right,
+                    color: AppColors.greyColor,
+                    size: 45,
+                  ),
+                ),
+                ListTile(
+                  leading: ImageIcon(
+                    AssetImage(AppIcons.transaction),
+                    color: AppColors.green,
+                  ),
+                  title: BoldText(
+                    text: 'Transactions',
+                    fontSize: 20,
+                    textColor: AppColors.blackColor,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  trailing: Icon(
+                    Icons.chevron_right,
+                    color: AppColors.greyColor,
+                    size: 45,
+                  ),
+                ),
+                ListTile(
+                  leading: ImageIcon(
+                    AssetImage(AppIcons.bell),
+                    color: AppColors.green,
+                  ),
+                  title: BoldText(
+                    text: 'Notifications',
+                    fontSize: 20,
+                    textColor: AppColors.blackColor,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  trailing: Icon(
+                    Icons.chevron_right,
+                    color: AppColors.greyColor,
+                    size: 45,
+                  ),
+                ),
+                ListTile(
+                  leading: ImageIcon(
+                    AssetImage(AppIcons.signout),
+                    color: AppColors.green,
+                  ),
+                  title: BoldText(
+                    text: 'Sign out',
+                    fontSize: 20,
+                    textColor: AppColors.blackColor,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  trailing: Icon(
+                    Icons.chevron_right,
+                    color: AppColors.greyColor,
+                    size: 45,
+                  ),
+                ),
+                // ListView.builder(
+                //   itemCount: contact.length,
+                //   itemBuilder: (context, index) {
+                //     return ListTile(
+                //       leading: ImageIcon(
+                //           AssetImage((contact[index].icons.toString()))),
+                //       title: NormalText(
+                //         text: contact[index].name.toString(),
+                //         textColor: AppColors.blackColor,
+                //       ),
+                //       trailing: Icon(
+                //         Icons.chevron_right,
+                //         color: AppColors.green,
+                //       ),
+                //     );
+                //   },
+                // ),
+              ],
+            )
           ],
         )
       ],
